31ad06773f1907064554ec553270911cf49a7426
Robert David Ramirez Garcia <robert.ramirez.garcia@gmail.com>
2022-05-08 21:20:54 -0500

    add configurations

---
+++ src/main/java/com/formacionbdi/springboot/app/gateway/springbootserviciogatewayserver/filter/CustomGlobalFilter.java
@@ -0,0 +1,34 @@
+package com.formacionbdi.springboot.app.gateway.springbootserviciogatewayserver.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class CustomGlobalFilter implements GlobalFilter {
+
+    private final Logger logger = LoggerFactory.getLogger(CustomGlobalFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        logger.info("ejecutando filtro pre");
+
+        return chain.filter(exchange).then(
+                Mono.fromRunnable(() ->
+                        {
+                            logger.info("ejecutando filtro post");
+                            exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "rojo").build());
+                            exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
+                        }
+                )
+        );
+    }
+}
+
